31ce82701cca1d5f240f945d2d6ec83a0cf3c5c7
Levirroh <johann.gr14@gmail.com>
2025-02-06 16:06:11 -0300

    feat: creating the de_to_by arquive

---
+++ Cibersecurity/projects/conversions/de_to_by.php
@@ -0,0 +1,34 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Decimal to Binary</title>
+</head>
+<body>
+    <h1>Conversor de Decimal em Binário.</h1>
+    <form method="POST">
+        <label for="decimal">Digite o número binário</label>
+        <input type="number" name="decimal">
+        <input type="submit" value="calcular" name="calcular">
+    </form>
+<?php
+
+if (isset($_POST['calcular'])){
+    $decimal = $_POST['decimal'];
+    $array = array_map('intval', str_split($decimal)); // faz com que toda a string de int seja dividida em item por item.
+    
+    // echo "<p>O número <b>". $binario ."</b> em decimal é: <b>". $soma."</b></p>";
+}
+
+
+/* mental map
+i=2 --> 1, true --> $soma = 0 + 2^0 = 1;
+
+i=1 --> 0, false --> $soma = 1;
+
+i=0 --> 1, true --> $soma = 1 + 2^2 = 5;
+*/
+
+?>    
+</body>
+</html>
